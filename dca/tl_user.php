6be25483a93443ea3aba9f765090487954e4ffcd
Marko Cupic <m.cupic@gmx.ch>
2018-01-04 23:36:16 +0100

    Add fields to tl_user with ContaoPaletteManipulator

--- dca/tl_user.php
+++ dca/tl_user.php
@@ -2,9 +2,9 @@
 
 /**
  * Contao Open Source CMS
- * 
+ *
  * Copyright (C) 2005-2012 Leo Feyer
- * 
+ *
  * @copyright  Marko Cupic 2012
  * @author     Marko Cupic, Oberkirch, Switzerland ->  mailto: m.cupic@gmx.ch
  * @package    be_email
@@ -37,10 +37,9 @@ $GLOBALS['TL_DCA']['tl_user']['fields']['alternate_email_2'] = array
     'sql' => "varchar(255) NOT NULL default ''"
 );
 
+
 // Add the fields to the palettes
-foreach ($GLOBALS['TL_DCA']['tl_user']['palettes'] as $key => $value) {
-    if ($key != '__selector__')
-    {
-        $GLOBALS['TL_DCA']['tl_user']['palettes'][$key] = str_replace('email', 'email,alternate_email,alternate_email_2', $GLOBALS['TL_DCA']['tl_user']['palettes'][$key]);
-    }
-}
+Contao\CoreBundle\DataContainer\PaletteManipulator::create()
+    ->addField(array('alternate_email', 'alternate_email_2'), 'email', Contao\CoreBundle\DataContainer\PaletteManipulator::POSITION_AFTER)
+    ->applyToPalette('default', 'tl_user')
+    ->applyToPalette('admin', 'tl_user');
