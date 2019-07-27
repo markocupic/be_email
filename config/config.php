a0a519fc5491d630e4903a13b9368b49fdecc3f8
Marko Cupic <m.cupic@gmx.ch>
2019-07-27 14:25:30 +0200

    Update config.php

--- config/config.php
+++ config/config.php
@@ -18,21 +18,21 @@ $GLOBALS['BE_MOD']['email']['tl_be_email'] = array
     'tables' => array('tl_be_email'),
 );
 
-// Add CSS
-$GLOBALS['TL_CSS'][] = 'system/modules/be_email/assets/stylesheet.css';
-
 if (TL_MODE == 'BE' && $_GET['do'] == 'tl_be_email')
 {
-    // http://leaverou.github.io/awesomplete
-    $GLOBALS['TL_JAVASCRIPT'][] = 'system/modules/be_email/assets/Autocompleter/awesomplete.js';
-    $GLOBALS['TL_CSS'][]        = 'system/modules/be_email/assets/Autocompleter/awesomplete.css';
+    $GLOBALS['TL_CSS'][] = 'system/modules/be_email/assets/stylesheet.css|static';
 
-    $GLOBALS['TL_JAVASCRIPT'][] = 'system/modules/be_email/assets/be_email_listing.js';
-    $GLOBALS['TL_JAVASCRIPT'][] = 'system/modules/be_email/assets/be_email_listing_autocomplete.js';
+    // http://leaverou.github.io/awesomplete
+    $GLOBALS['TL_JAVASCRIPT'][] = 'system/modules/be_email/assets/Autocompleter/awesomplete.js|static';
+    $GLOBALS['TL_CSS'][]        = 'system/modules/be_email/assets/Autocompleter/awesomplete.css|static';
+
+    $GLOBALS['TL_JAVASCRIPT'][] = 'system/modules/be_email/assets/be_email_listing.js|static';
+    $GLOBALS['TL_JAVASCRIPT'][] = 'system/modules/be_email/assets/be_email_listing_autocomplete.js|static';
+
     if ($_GET['act'] == 'edit')
     {
         // Add Javascript
-        $GLOBALS['TL_JAVASCRIPT'][] = 'system/modules/be_email/assets/be_email.js';
+        $GLOBALS['TL_JAVASCRIPT'][] = 'system/modules/be_email/assets/be_email.js|static';
     }
 
     // HOOKS
